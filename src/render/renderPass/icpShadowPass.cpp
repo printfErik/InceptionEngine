cc7a40d9c4b3d38a14574bada0efccdec3e20047
Erik Bi <erik_3822@outlook.com>
2023-12-13 17:58:57 +0800

    Update icpShadowPass.cpp

--- src/render/renderPass/icpShadowPass.cpp
+++ src/render/renderPass/icpShadowPass.cpp
@@ -194,7 +194,19 @@ void icpShadowPass::SetupPipeline()
 
 void icpShadowPass::CreateShadowRenderPass()
 {
-	
+	std::array<VkAttachmentDescription, 1> attachments{};
+
+	// Depth attachment
+	attachments[0].format = icpVulkanUtility::findDepthFormat(m_rhi->GetPhysicalDevice());
+	attachments[0].samples = VK_SAMPLE_COUNT_1_BIT;
+	attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	attachments[0].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+
 }
 
 void icpShadowPass::CreateShadowAttachment()
